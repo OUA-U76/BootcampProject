f9ea9b1c01b8137090f0769f491a67dd93659b1d
Ezgi Orman <ezgiorman10@gmail.com>
2023-06-18 13:46:33 +0300

    added "Controller" script and walking animation.

---
+++ BootcampProject/Assets/Scripts/Controller.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{ 
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("w"))
+            animator.SetBool("isWalking", true);
+        else
+            animator.SetBool("isWalking", false);
+    }
+}
