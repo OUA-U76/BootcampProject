57daa854bb5e5dd98d8645d8f870011cbdc8a6b5
“Berk” <beerkaydemir@gmail.com>
2023-07-15 17:54:33 +0300

    UI Changes

--- BootcampProject/Assets/Scripts/StartScreenUIController.cs
+++ BootcampProject/Assets/Scripts/StartScreenUIController.cs
@@ -3,21 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.Serialization;
 
 public class StartScreenUIController : MonoBehaviour
 {
 
 [SerializeField] Button OptionsButton;
 [SerializeField] Button OkButton;
+[SerializeField] Button OkLeaderBoardButton;
 [SerializeField] Button QuitButton;
 [SerializeField] Button StartButton;   
+[SerializeField] Button leaderBoardButton;
 [SerializeField] Animator OptionsAnimator;
+[SerializeField] Animator LeaderBoardAnimator;
 bool OptionsOpen = false;
+bool LeaderBoardOpen = false;
 
 private void Start() 
 {
     OptionsButton.onClick.AddListener(OptionsButtonBehaviour);
+    leaderBoardButton.onClick.AddListener(LeaderBoardButtonBehaviour);
     OkButton.onClick.AddListener(CloseOptions);
+    OkLeaderBoardButton.onClick.AddListener(CloseLeaderBoard);
     QuitButton.onClick.AddListener(QuitGame);
     StartButton.onClick.AddListener(StartGame);
 }
@@ -29,7 +36,34 @@ void OptionsButtonBehaviour(){
     }
     else
     {
-        OpenOptions();
+        if (LeaderBoardOpen)
+        {
+            CloseLeaderBoard();
+            OpenOptions();
+        }
+        else
+        {
+            OpenOptions();
+        }
+    }
+}
+
+void LeaderBoardButtonBehaviour(){
+    if(LeaderBoardOpen)
+    {
+        CloseLeaderBoard();
+    }
+    else
+    {
+        if (OptionsOpen)
+        {
+            CloseOptions();
+            OpenLeaderBoard();
+        }
+        else
+        {
+            OpenLeaderBoard();
+        }
     }
 }
 
@@ -38,10 +72,22 @@ void OpenOptions()
     OptionsOpen = true;
     OptionsAnimator.SetBool("isOpen", true);
 }
+
+void OpenLeaderBoard()
+{
+    LeaderBoardOpen = true;
+    LeaderBoardAnimator.SetBool("isOpen", true);
+}
 void CloseOptions()
 {
     OptionsOpen = false;
     OptionsAnimator.SetBool("isOpen", false);
+}
+
+void CloseLeaderBoard()
+{
+    LeaderBoardOpen = false;
+    LeaderBoardAnimator.SetBool("isOpen", false);
 }
 void QuitGame()
 {
